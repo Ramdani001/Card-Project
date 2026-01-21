0a194777c880ed7d5aaf0dd14c5fbc80e7c7007e
Fidra Aulia Asana <fidraauliaa@gmail.com>
2026-01-22 03:41:50 +0700

    Penambahan API users

---
+++ app/lib/server/prisma.config.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@/app/generated/prisma";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma =
+  globalThis.prisma ??
+  new PrismaClient({
+    log: ["query", "info", "warn", "error"], // opsional
+  });
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
