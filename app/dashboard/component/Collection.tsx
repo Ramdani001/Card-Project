9408cd4a9122bb256028839a8e7aa7db9b0d017d
Fidra Aulia Asana <fidraauliaa@gmail.com>
2026-02-06 20:59:57 +0700

    Penambahan route collection dashboar

--- app/dashboard/component/Collection.tsx
+++ app/dashboard/component/Collection.tsx
@@ -1,237 +1,29 @@
 "use client";
 
-import { AppDispatch, RootState } from "@/app/reudux/store";
-import { Container, Table, Pagination, Modal, Stack, TextInput, Combobox, InputBase, useCombobox, Input, FileInput, Button, Flex, Center } from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
-import { useEffect, useState } from "react";
-import axios from 'axios'
-import { fetchData } from "next-auth/client/_utils";
-
-const groceries = ['🍎 Apples', '🍌 Bananas', '🥦 Broccoli', '🥕 Carrots', '🍫 Chocolate'];
+import { Button, Flex, Grid } from "@mantine/core";
+import Card from "./Card";
+import Type from "./Type";
+import Discount from "./Discount";
 
 const Collection = () => {
-  const [activePage, setActivePage] = useState(1);
-  const [opened, { open, close }] = useDisclosure(false);
-  const [elements, setElements] = useState([])
-  const rowsPerPage = 10;
-
-  const startIndex = (activePage - 1) * rowsPerPage;
-  const endIndex = startIndex + rowsPerPage;
-useEffect(() => {
-  const fetchData = async () => {
-    try {
-      const url = "http://localhost:3000/api/cards?page=1&limit=5";
-      const response = await axios.get(url);
-      setElements(response.data.data);
-    } catch (error) {
-      console.error("Fetch error:", error);
-    }
-  };
-
-  fetchData();
-}, []);
-
-useEffect(() => {
-  console.log(elements);
-}, [elements]);
-const currentRows = elements.slice(startIndex, endIndex);
-
-const handleEdit = ((idCard) => {
-    alert(idCard);
-})
-
-const handleDelete = ((idCard) => {
-    alert(idCard);
-})
-
-const rows = currentRows.map((item) => (
-  <Table.Tr key={item.idCard}>
-    {/* Image */}
-    <Table.Td>
-      <img
-        src={item.detail.image.location}
-        alt={item.detail.name}
-        width={60}
-      />
-    </Table.Td>
-
-    {/* Name (detail) */}
-    <Table.Td>{item.detail.name}</Table.Td>
-
-    {/* Type Card */}
-    <Table.Td>{item.typeCard.name}</Table.Td>
-
-    {/* Price */}
-    <Table.Td>
-      Rp {item.detail.price.toLocaleString("id-ID")}
-    </Table.Td>
-
-    {/* Stock */}
-    <Table.Td>{item.detail.stock}</Table.Td>
-
-    {/* Discount */}
-    <Table.Td>{item.detail.discount.discount}%</Table.Td>
-
-    {/* Action */}
-    <Table.Td>
-        <Flex gap="md">
-            <Button variant="filled" onClick={() => handleEdit(item.idCard)}>
-                Edit
-            </Button>
-            <Button variant="filled" color="red" onClick={() => handleDelete(item.idCard)}>
-                Delete
-            </Button>
-        </Flex>
-    </Table.Td>
-  </Table.Tr>
-));
-
-
-    const combobox = useCombobox({
-        onDropdownClose: () => combobox.resetSelectedOption(),
-    });
-
-    const typeCard = useCombobox({
-        onDropdownClose: () => typeCard.resetSelectedOption(),
-    });
-
-    const [typeVal, setTypeVal] = useState<string | null>(null);
-    const [value, setValue] = useState<string | null>(null);
-
-    const options = groceries.map((item) => (
-        <Combobox.Option value={item} key={item}>
-        {item}
-        </Combobox.Option>
-    ));
-
-    const optionType = groceries.map((item) => (
-        <Combobox.Option value={item} key={item}>
-        {item}
-        </Combobox.Option>
-    ));
-
+ 
   return (
     <>
-        <Container style={{ minWidth: "80vw", minHeight: "88vh" }}>
-            <Button variant="filled" onClick={open}>Add Card</Button>  
-
-            <Table stickyHeader stickyHeaderOffset={60}>
-                <Table.Thead>
-                <Table.Tr>
-                    <Table.Th>Image</Table.Th>
-                    <Table.Th>Name</Table.Th>
-                    <Table.Th>Type</Table.Th>
-                    <Table.Th>Price</Table.Th>
-                    <Table.Th>Stock</Table.Th>
-                    <Table.Th>Discount</Table.Th>
-                </Table.Tr>
-                </Table.Thead>
-                <Table.Tbody>{rows}</Table.Tbody>
-            </Table>
-
-            <Pagination
-                total={Math.ceil(elements.length / rowsPerPage)}
-                page={activePage}
-                onChange={setActivePage}
-                position="center"
-                color="blue"
-                style={{ marginTop: "20px" }}
-            />
-            <Modal opened={opened} onClose={close} title="Add Collection">
-                <form>
-                    <Stack>
-                        <TextInput
-                        label="Name"
-                        placeholder="Yu-Gi oh"
-                        radius="md"
-                        />
-
-                        {/* Type Card */}
-                        <div>
-                            <label>Type Card</label>
-                            <Combobox
-                            store={typeCard}
-                            onOptionSubmit={(val) => {
-                                setTypeVal(val);
-                                typeCard.closeDropdown();
-                            }}
-                            >
-                                <Combobox.Target>
-                                    <InputBase
-                                    component="button"
-                                    type="button"
-                                    pointer
-                                    rightSection={<Combobox.Chevron />}
-                                    rightSectionPointerEvents="none"
-                                    onClick={() => typeCard.toggleDropdown()}
-                                    >
-                                    {typeVal || <Input.Placeholder>Type Card</Input.Placeholder>}
-                                    </InputBase>
-                                </Combobox.Target>
-
-                                <Combobox.Dropdown>
-                                    <Combobox.Options>{optionType}</Combobox.Options>
-                                </Combobox.Dropdown>
-                            </Combobox>
-                        </div>
-
-                        <TextInput
-                        label="Price"
-                        placeholder="Yu-Gi oh"
-                        radius="md"
-                        />
-                        <TextInput
-                        label="Stok"
-                        placeholder="Yu-Gi oh"
-                        radius="md"
-                        />
-
-                        {/* Discount */}
-                        <div>
-                            <label>Discount</label>
-                            <Combobox
-                            store={combobox}
-                            onOptionSubmit={(val) => {
-                                setValue(val);
-                                combobox.closeDropdown();
-                            }}
-                            >
-                                <Combobox.Target>
-                                    <InputBase
-                                    component="button"
-                                    type="button"
-                                    pointer
-                                    rightSection={<Combobox.Chevron />}
-                                    rightSectionPointerEvents="none"
-                                    onClick={() => combobox.toggleDropdown()}
-                                    >
-                                    {value || <Input.Placeholder>Discount</Input.Placeholder>}
-                                    </InputBase>
-                                </Combobox.Target>
-
-                                <Combobox.Dropdown>
-                                    <Combobox.Options>{options}</Combobox.Options>
-                                </Combobox.Dropdown>
-                            </Combobox>
-                        </div>
-
-                         <FileInput label="Image" placeholder="Upload files" multiple />
-
-                    </Stack>
-
-                    <Flex
-                        pt="md"
-                        gap="xl"
-                        align="center"
-                    >
-                        <Button variant="filled" fullWidth>Submit</Button>
-                    </Flex>
-
-                </form>
-            </Modal>
-        </Container>
-
-
+        <Grid style={{ width: "80vw" }}>
+            <Grid.Col>
+                <Grid>
+                    <Grid.Col span={6}>
+                        <Type />
+                    </Grid.Col>
+                    <Grid.Col span={6}>
+                        <Discount />
+                    </Grid.Col>
+                </Grid>
+            </Grid.Col>
+            <Grid.Col>
+                <Card />
+            </Grid.Col>
+        </Grid>
     </>
   );
 };
---
+++ app/dashboard/component/Discount.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { Button, Pagination, Table } from "@mantine/core";
+import { useState } from "react";
+
+const initialElements = [
+  { position: 6, mass: 12.011, symbol: "C", name: "Carbon" },
+  { position: 7, mass: 14.007, symbol: "N", name: "Nitrogen" },
+  { position: 39, mass: 88.906, symbol: "Y", name: "Yttrium" },
+  { position: 56, mass: 137.33, symbol: "Ba", name: "Barium" },
+  { position: 58, mass: 140.12, symbol: "Ce", name: "Cerium" },
+];
+
+const Discount = () => {
+  const [activePage, setActivePage] = useState(1);
+  const [elements] = useState(initialElements);
+
+  const rowsPerPage = 5;
+  const startIndex = (activePage - 1) * rowsPerPage;
+  const endIndex = startIndex + rowsPerPage;
+  const currentRows = elements.slice(startIndex, endIndex);
+
+  const rows = currentRows.map((item) => (
+    <Table.Tr key={item.position}>
+      <Table.Td>{item.position}</Table.Td>
+      <Table.Td>{item.name}</Table.Td>
+      <Table.Td>{item.symbol}</Table.Td>
+      <Table.Td>{item.mass}</Table.Td>
+    </Table.Tr>
+  ));
+
+  return (
+      <>
+      <Button variant="filled">Add Discount</Button>
+      <Table>
+        <Table.Thead>
+          <Table.Tr>
+            <Table.Th>Element position</Table.Th>
+            <Table.Th>Element name</Table.Th>
+            <Table.Th>Symbol</Table.Th>
+            <Table.Th>Atomic mass</Table.Th>
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>{rows}</Table.Tbody>
+      </Table>
+
+      <Pagination
+        total={Math.ceil(elements.length / rowsPerPage)}
+        page={activePage}
+        onChange={setActivePage}
+        position="center"
+        color="blue"
+        style={{ marginTop: "20px" }}
+      />
+    </>
+  );
+};
+
+export default Discount;
