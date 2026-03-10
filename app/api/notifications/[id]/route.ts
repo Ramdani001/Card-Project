e749ecdc91c85435e28483e6c71c5017427786cb
Prasetyono Putra <prasetyonoptr@gmail.com>
2026-03-10 19:44:04 +0700

    delete notif v2

--- app/api/notifications/[id]/route.ts
+++ app/api/notifications/[id]/route.ts
@@ -1,11 +1,12 @@
+import { NextRequest } from "next/server"; // Import NextRequest
 import { handleApiError, sendResponse } from "@/helpers/response.helper";
 import { deleteNotification } from "@/services/transaction/notification.service";
 
-type RouteParams = {
+type RouteContext = {
   params: Promise<{ id: string }>;
 };
 
-export const DELETE = async ({ params }: RouteParams) => {
+export const DELETE = async (request: NextRequest, { params }: RouteContext) => {
   try {
     const { id } = await params;
 
