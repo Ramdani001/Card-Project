75264707c5e7240223d080dfc24867f077bb76b8
Prasetyono Putra <prasetyonoptr@gmail.com>
2026-04-03 15:01:04 +0700

    adjust ecosystem.config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,11 @@ module.exports = {
   apps: [
     {
       name: "card-app",
-      script: "npm",
+      script: "./node_modules/next/dist/bin/next",
       args: "start",
       cwd: "./",
+      exec_mode: "cluster",
+      instances: "max",
       autorestart: true,
       watch: false,
       max_memory_restart: "1G",
