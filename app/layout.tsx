100f379ca22e4040c3cf91cc1a802a5dfeb578a8
Rizkan Ramdani <rizkan0990@gmail.com>
2026-06-21 12:12:16 +0700

    Update untuk ads google
    Update untuk ads google

--- app/layout.tsx
+++ app/layout.tsx
@@ -90,22 +90,14 @@ export default function RootLayout({
         {/* Google Tag Manager */}
         <Script id="gtm-script" strategy="afterInteractive">
           {`
-            (function(w,d,s,l,i){
-              w[l]=w[l]||[];
-              w[l].push({
-                'gtm.start': new Date().getTime(),
-                event:'gtm.js'
-              });
-
-              var f=d.getElementsByTagName(s)[0],
-                  j=d.createElement(s),
-                  dl=l!='dataLayer'?'&l='+l:'';
-
-              j.async=true;
-              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-
+            (
+            function(w,d,s,l,i)
+              {w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
               f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-WLKMGL46');
+              }
+            )(window,document,'script','dataLayer','GTM-NWGTW53Q')
           `}
         </Script>
 
@@ -153,15 +145,15 @@ export default function RootLayout({
         </Suspense>
 
         <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-WLKMGL46"
-            height="0"
-            width="0"
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NWGTW53Q"
+            height="0" 
+            width="0" 
             style={{
               display: "none",
               visibility: "hidden",
             }}
-          />
+            />
         </noscript>
 
         <noscript>
